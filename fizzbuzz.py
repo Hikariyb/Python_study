b4bbe04e3f0df82ca8b218a6565d3ea841deb82d
Hikariyb <c1508118@st.kanazawa-it.ac.jp>
2025-07-06 19:23:02 +0900

    FizzBuzzの表示

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,10 +1,12 @@
 #FizzBuzz(3の倍数ならばFizz,5の倍数ならばBuzz,3及び5の両方の倍数ならばFizzBuzzを表示し、それ以外ならば数字を表示するプログラム)を作ろう！！
 num = int(input("最後の数字を入力してください。"))
 
-for i in range(0:num+1):
-    if ():
+for i in range(1, num+1):
+    if i % 3 == 0 and i % 5 == 0:
         print("FizzBuzz")
-    elif ():
+    elif i % 3 == 0:
         print("Fizz")
-    else:
+    elif i % 5 == 0:
         print("Buzz")
+    else:
+        print(i)
