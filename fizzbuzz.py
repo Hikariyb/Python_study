973ee424df2a913b670b43836f588856339268ec
rikut0904 <rkt9tuba4robot@gmail.com>
2025-07-06 19:12:19 +0900

    add/FizzBuzz問題

---
+++ fizzbuzz.py
@@ -0,0 +1,10 @@
+#FizzBuzz(3の倍数ならばFizz,5の倍数ならばBuzz,3及び5の両方の倍数ならばFizzBuzzを表示し、それ以外ならば数字を表示するプログラム)を作ろう！！
+num = int(input("最後の数字を入力してください。"))
+
+for i in range(0:num+1):
+    if ():
+        print("FizzBuzz")
+    elif ():
+        print("Fizz")
+    else:
+        print("Buzz")
